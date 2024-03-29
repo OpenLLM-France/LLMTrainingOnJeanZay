f36e846d9a09b3740af699c321da54c77f7c48e4
Christophe Cerisara <cerisara@loria.fr>
2024-03-29 09:48:10 +0100

    shuffling much faster on nvme

--- src/shuf.py
+++ src/shuf.py
@@ -1,6 +1,7 @@
 import sqlite3
+import time
 
-temp_db = "utts.sqlite"
+temp_db = "/home/xtof/nvme/openllm/utts.sqlite"
 connection = sqlite3.connect(temp_db)
 cursor = connection.cursor()
 cursor.execute("""
@@ -10,12 +11,14 @@ cursor.execute("""
 """)
 n=0
 putt=[]
-with open("wikifr.toks","r") as fp:
+t0=time.time()
+with open("/home/xtof/nvme/openllm/wikifr.toks","r") as fp:
     for line in fp:
         if line[0]=='Y':
             utt = [int(x) for x in line[2:].split(' ') if len(x)>0]
             n+=len(utt)
-            print("NTOKS",n)
+            t1=time.time()
+            print("NTOKS",n,t1-t0)
             if len(utt)>0: putt += utt
             while len(putt)>=2048:
                 sutt = ' '.join([str(x) for x in putt[:2048]])
@@ -23,7 +26,7 @@ with open("wikifr.toks","r") as fp:
                 connection.commit()
                 putt = putt[2048:]
 
-with open("shuffled.toks", "w") as fp:
+with open("/home/xtof/nvme/openllm/shuffled.toks", "w") as fp:
   for line in cursor.execute("""
       SELECT line FROM lines ORDER BY random();
       """):
