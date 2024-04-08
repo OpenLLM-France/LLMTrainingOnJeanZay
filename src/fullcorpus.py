b5f4c627083164336272053c0276f4b9c2540333
Christophe Cerisara <cerisara@loria.fr>
2024-04-08 16:25:16 +0200

    try single file to evaluate seek()

--- src/fullcorpus.py
+++ src/fullcorpus.py
@@ -10,24 +10,14 @@ TODO:
 - pas besoin de arrow, python seek() est O(1)
 """
 
-outdir = "alldata/"
-fnoms, tgtnoms=[],[]
-with open("fnoms","r") as f:
-    for l in f:
-        ss = l.strip().split("\t")
-        fnoms.append(ss[0])
-        tgtnoms.append(ss[1])
-for i in range(len(fnoms)):
-    fnom = fnoms[i]
-    pf = pq.ParquetFile(fnom)
-    ff = outdir+tgtnoms[i]+".txt"
-    with open(ff+".idx","wb") as fi:
-        with open(ff,"w") as f:
+with open("alldata/all.txt","w") as f:
+    with open("fnoms","r") as g:
+        for l in g:
+            fnom = l.strip()
+            pf = pq.ParquetFile(fnom)
+            print("FFNOM__",fnom,f.tell())
             for data in pf.iter_batches():
                 txtcol = [str(x) for x in data.column_names if 'text' in x or 'txt' in x]
-                print(txtcol)
                 for x in data[txtcol[0]]:
                     f.write(str(x)+'\n')
-                    pickle.dump(f.tell(),fi)
-                print("FFNOM__",fnom,len(data))
 
--- src/fullcorpus.sh
+++ src/fullcorpus.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 scp fullcorpus.* jeanzay:/gpfswork/rech/knb/uyr14tk/home/openllmfr/
-scp fnoms jeanzay:/gpfswork/rech/knb/uyr14tk/home/openllmfr/
 
 ssh jeanzay << 'ENDLULLY'
 cd /gpfswork/rech/knb/uyr14tk/home/openllmfr/
+rm fnoms
+find /gpfswork/rech/qgz/commun/data/corpus_openllm/ -name "*.parquet" | grep -v -e perplexity_corpus_open_llm > fnoms
 sbatch fullcorpus.slurm
 ENDLULLY
 
