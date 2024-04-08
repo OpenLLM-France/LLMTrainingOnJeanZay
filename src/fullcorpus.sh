b51e56631cba1470f1a10683d8dd93efb7baaf4f
Christophe Cerisara <cerisara@loria.fr>
2024-04-08 14:57:37 +0200

    ok

--- src/fullcorpus.sh
+++ src/fullcorpus.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 scp fullcorpus.* jeanzay:/gpfswork/rech/knb/uyr14tk/home/openllmfr/
+scp fnoms jeanzay:/gpfswork/rech/knb/uyr14tk/home/openllmfr/
 
 ssh jeanzay << 'ENDLULLY'
 cd /gpfswork/rech/knb/uyr14tk/home/openllmfr/
