e37925a75ab0cf5591e812f7325ad012e81e60a1
Christophe Cerisara <cerisara@loria.fr>
2024-04-10 07:58:59 +0200

    ok

--- src/fullcorpus.py
+++ src/fullcorpus.py
@@ -11,17 +11,24 @@ TODO:
 - pas besoin de arrow, python seek() est O(1)
 """
 
+with open("alldata/idx.txt","r") as f:
+    done = [s.split()[1] for s in f if s.startswith("FFNOM__")]
+print("DONE",len(done))
+
 t0 = time.time()
-with open("alldata/all.txt","w") as f:
+with open("alldata/all.txt","a") as f:
     with open("fnoms","r") as g:
         for l in g:
             fnom = l.strip()
+            if fnom in done:
+                print("skip",fnom)
+                continue
             pf = pq.ParquetFile(fnom)
             t1 = time.time()
             print("FFNOM__",fnom,f.tell(), t1-t0)
             t0 = t1
             for data in pf.iter_batches():
-                txtcol = [str(x) for x in data.column_names if 'text' in x or 'txt' in x]
+                txtcol = [str(x) for x in data.column_names if 'text' in x or 'txt' in x or 'content' in x]
                 for x in data[txtcol[0]]:
                     f.write(str(x)+'\n')
 
