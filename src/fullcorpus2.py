60103cae2dc85d26d9b6f8e551e39e3672be24cd
xtof <cerisara@loria.fr>
2024-04-18 21:05:14 +0200

    pb tokenizer trop lent

--- src/fullcorpus2.py
+++ src/fullcorpus2.py
@@ -16,6 +16,10 @@ tokenizer.padding_side = 'left'
 
 # TODO: speedup by tokenizing utts in parallel
 
+t0 = time.time()
+ttok = 0
+tsav = 0
+nseq = 0
 with open("alldata/all.pkl","wb") as g:
     with open("alldata/all.idx","wb") as gidx:
         with open("alldata/all.txt","r") as f:
@@ -23,15 +27,27 @@ with open("alldata/all.pkl","wb") as g:
             cur = []
             for i,l in enumerate(f):
                 lines.append(l)
-                if len(lines)>10:
+                if len(lines)>1000:
+                    t1 = time.time()
                     ltoks = tokenizer(lines, return_tensors=None)
+                    t2 = time.time()
+                    ttok += t2-t1
                     # already concat to 2048 + index
                     for toks in ltoks['input_ids']: cur.append(toks)
                     while len(cur)>=2048:
+                        t1 = time.time()
+                        nseq += 1
                         pickle.dump(g.tell(),gidx)
                         pickle.dump(cur[:2048],g)
+                        t2 = time.time()
+                        tsav += t2-t1
                         cur = cur[2048:]
-                    print("l",i,len(ltoks['input_ids']),' '.join([str(len(x)) for x in ltoks['input_ids']]))
+                    mtokperh = (float(nseq)*3.600*2.048)/float(t2-t0)
+                    # millions of tokens per hour
+                    # la majorite du temps est pris par le tokenizer: 760s vs. 21s pour le save pour un total de 840s
+                    # PB1: trop lent: 61Mtoks/h alors que HF tokenizers fast run a 50Mtoks/s
+                    # PB2: la vitesse decroit lentement
+                    print("l",i,mtokperh,ttok,tsav,len(ltoks['input_ids']))
                     gidx.flush()
                     g.flush()
                     lines = []
