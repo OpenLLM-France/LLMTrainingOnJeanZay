74e53b94d7081e74637daaab86c227c0812249af
xtof <cerisara@loria.fr>
2024-04-20 06:50:02 +0200

    pickle too slow

--- src/fullcorpus2.py
+++ src/fullcorpus2.py
@@ -1,11 +1,25 @@
 import pickle
+import time
 
 # 23738037750 lignes
 # 542859000 en 2h = 2% ==> 100h = 4 jours
+# 5937339000 en 20h = 25% ==> 100h = 80h = 3.4 jours
 
-with open("alldata/all.idx","wb") as gidx:
+# PB: combien de temps prend la lecture de tous les index ? et quelle RAM ?
+t0 = time.time()
+with open("alldata/all.idx","rb") as gidx:
+    i=0
+    while True:
+        try:
+            idx=pickle.load(gidx)
+            i+=1
+        except: break
+t1 = time.time()
+print("last record",i,idx,t1-t0)
+
+with open("alldata/all.idx","ab") as gidx:
     with open("alldata/all.txt","r") as f:
-        i=0
+        f.seek(idx)
         l=f.readline()
         while l:
             pickle.dump(f.tell(),gidx)
--- src/testcorpus.py
+++ src/testcorpus.py
@@ -1,7 +1,13 @@
 import pickle
+import time
+import random
+
+# 23738037750 lignes, il faut 90s pour en lire 38000, donc pour tout lire == 650 jours !!! 
+# pickle est bcp trop lent, j'abandonne l'idee de conserver des index, mieux vaut faire un seek random direct dans le texte
 
 idx = {0:0}
 j=0
+t0=time.time()
 with open("alldata/all.txt","r") as g:
     with open("alldata/all.idx","rb") as f:
         while True:
@@ -9,7 +15,18 @@ with open("alldata/all.txt","r") as g:
                 i = pickle.load(f)
                 idx[j]=i
                 j+=1
+                if j%1000==0:
+                    t1=time.time()
+                    print("T",len(idx),t1-t0)
             except: break
-            g.seek(idx[j-1])
-            s=g.readline()
-            print("N",len(idx),s)
+    t1=time.time()
+    print("N",len(idx),t1-t0)
+
+    r = [i for i in range(len(idx))]
+    random.shuffle(r)
+    for i in r[:100]:
+        t1=time.time()
+        g.seek(idx[i])
+        t2=time.time()
+        s=g.readline()
+        print("R",i,t2-t1,s)
