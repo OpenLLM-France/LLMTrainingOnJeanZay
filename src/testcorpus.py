71fb9e4f342558b52f350fad205a1d6e4df3e9e3
xtof <cerisara@loria.fr>
2024-04-20 07:14:55 +0200

    ok pour lire le corpus en random avec des seek, mais le corpus n'a pas l'air top, allemand, espagnol, bcp de fautes d OCR, FR trop ancien, des series de chiffres

--- src/testcorpus.py
+++ src/testcorpus.py
@@ -1,32 +1,26 @@
-import pickle
 import time
 import random
+import os
 
-# 23738037750 lignes, il faut 90s pour en lire 38000, donc pour tout lire == 650 jours !!! 
-# pickle est bcp trop lent, j'abandonne l'idee de conserver des index, mieux vaut faire un seek random direct dans le texte
+# ceci permet de lire le corpus au hasard, mais les resultats sont plutot decevants: pas sur que le corpus soit tres bon!
 
-idx = {0:0}
-j=0
+txtf = "alldata/all.txt"
+l=os.path.getsize(txtf)
+sp = ord(' ')
+print("filesize",l,sp)
+l -= 50000
+
 t0=time.time()
-with open("alldata/all.txt","r") as g:
-    with open("alldata/all.idx","rb") as f:
-        while True:
-            try:
-                i = pickle.load(f)
-                idx[j]=i
-                j+=1
-                if j%1000==0:
-                    t1=time.time()
-                    print("T",len(idx),t1-t0)
-            except: break
-    t1=time.time()
-    print("N",len(idx),t1-t0)
-
-    r = [i for i in range(len(idx))]
-    random.shuffle(r)
-    for i in r[:100]:
+with open(txtf,"rb") as g:
+    for i in range(100):
+        d = random.randrange(l)
         t1=time.time()
-        g.seek(idx[i])
+        g.seek(d)
         t2=time.time()
-        s=g.readline()
-        print("R",i,t2-t1,s)
+        while True:
+            c=g.read(1)
+            if ord(c)==sp: break
+        # g.read(1)
+        s = g.readline()
+        s = s.decode("utf-8")
+        print("R",i,t2-t1,len(s),s)
